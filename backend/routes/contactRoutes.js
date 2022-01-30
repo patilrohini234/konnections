35c2eecd3cc29d2c2db023d9733d361b1eb60e7e
patilrohini234 <patilrohini234@gmail.com>
2022-01-30 23:34:49 +0530

    added dockerfile

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { protect } = require("../middleware/authMiddleware.js");
+const { protect } = require("../middleware/authmiddleware.js");
 const { searchByName, getByContactNumber } = require("../controllers/contacts");
 
 router.route("/").get(protect, searchByName);
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 const config = {
-  BACKEND_HOST: "http://localhost:4000",
+  BACKEND_HOST: "http://54.92.133.115:4000",
 };
 module.exports = config;
